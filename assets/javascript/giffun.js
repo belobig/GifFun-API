b25dd92c90380171271dc9f3925dcd5bbd409ef5
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-02-27 08:34:14 -0700

    mostly there, just need to fix form and rating paragraphs

---
+++ assets/javascript/giffun.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+
+	var topics = ['the legend of zelda', 'super mario bros', 'tekken', 'street fighter', 'mortal kombat', 'Halo: Combat Evolved', 'zork', 'clash of clans', 'starcraft', 'world of warcraft'];
+
+	var searchTerm = 'video games';
+	var apiKey = 'yXCvgqoPDZar0CdVaI1gXpmg9HErARJf';
+	var imgAnimateURL = '';
+	var imgStillURL = '';
+	var image = '';
+
+	// Display buttons at the top of the page
+	function showButtons() {
+
+		// Deleting buttons prior to adding new buttons to avoid repeats
+		$("#buttons").empty();
+
+		// Looping through the array of button titles
+		for (let h = 0; h < topics.length; h++) {
+			var button = $("<button>");
+			button.addClass("gifBtn btn btn-kelly");
+			button.text(topics[h]);
+			$("#buttons").append(button);
+		}
+	}
+
+	// Function to display Gif Info
+	function displayGIFinfo() {
+		var searchTerm = $(this).text();
+		var queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + searchTerm + '&limit=10&lang=en';
+
+		$("#gifArea").empty();
+
+		$.ajax({
+			url: queryUrl,
+			method: "GET"
+		}).then(function (response) {
+			console.log(response);
+
+
+			var rating = '';
+			var pRating = '';
+
+			for (let i = 0; i < response.data.length; i++) {
+				imgStillURL = response.data[i].images.fixed_width_still.url;
+				imgAnimateURL = response.data[i].images.fixed_width.url;
+				image = $("<img>").attr("src", imgStillURL);
+				image.addClass("img-rounded buffer gifs");
+				image.attr("data-still", imgStillURL);
+				image.attr("data-animate", imgAnimateURL);
+				image.attr("data-state", "still");
+				rating = response.data[i].rating;
+				pRating = $("<p>").text("Rating: " + rating);
+				$("#gifArea").append(image);
+				$("#gifs").append(pRating);
+			}
+
+		});
+	}
+
+	// Click event listener to add gifs to page when a button from the list at the top is clicked
+	$(document).on("click", ".gifBtn", displayGIFinfo);
+
+	// Function to determine if gif is paused or animated, and change accordingly
+	function gifClick() {
+		var currentGif = $(this);
+		var state = currentGif.attr("data-state");
+		var animateUrl = currentGif.attr("data-animate");
+		var stillUrl = currentGif.attr("data-still");
+		// console.log("Gif clicked");
+		if (state === "still") {
+			currentGif.attr("src", animateUrl);
+			currentGif.attr("data-state", "animate");
+		} else {
+			currentGif.attr("src", stillUrl);
+			currentGif.attr("data-state", "still");
+		}
+	}
+
+	// Click listener on each gif to pause or unpause
+	$(document).on("click", ".gifs", gifClick);
+
+
+
+	showButtons();
+
+});
